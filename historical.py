9012e3431c3ea8ea6a3337eaa2107805334a0d42
indigoscot291-lab <indigoscot291@gmail.com>
2026-08-10 10:17:26 -0400

    Update historical.py

--- historical.py
+++ historical.py
@@ -6,3 +6,8 @@ import re
 import io
 import concurrent.futures
 from pymongo import MongoClient
+
+@st.cache_resource
+def get_mongo():
+    uri = st.secrets["MONGO_URI"]
+    return MongoClient(uri)
